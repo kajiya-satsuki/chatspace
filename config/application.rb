67e19f8d666c7d69144c7f8a5847e250c53bf04e
kajiya-satsuki <shupeen.soykiki@gmail.com>
2018-08-06 21:38:49 +0900

    inplemented flash message and replaced English with Japanese

--- config/application.rb
+++ config/application.rb
@@ -2,23 +2,19 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Chatspace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     config.generators do |g|
       g.javascripts false
       g.helper false
       g.test_framework false
+
+    config.i18n.default_locale = :ja
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end

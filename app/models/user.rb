7e57dd78d9cdc4c33d0cfa1020ee1709fc3b8654
kajiya-satsuki <shupeen.soykiki@gmail.com>
2018-09-17 16:49:42 +0900

    create group controller and model and set routing for groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :messages
+  has_many :groups, through: :group_users
+  has_many :group_users
+  
   validates :name, :email, presence: true, uniqueness: true
 end

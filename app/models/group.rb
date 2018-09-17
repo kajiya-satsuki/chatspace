66463c9f133c6834a586456eaa1e731d6f64b887
kajiya-satsuki <shupeen.soykiki@gmail.com>
2018-09-17 18:06:06 +0900

    add uniqueness: true to name column in group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
-  validates :name, presence: true
+  validates :name, presence: true, uniquness: :true
 end

5eb8b637af4c2250df9fc4205bf2be1a129fede1
kajiya-satsuki <shupeen.soykiki@gmail.com>
2018-10-18 15:36:27 +0900

    create groups_controller, group model, and view screen, and set the routing for groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages
-  has_many :groups, through: :group_users
   has_many :group_users
-  
+  has_many :groups, through: :group_users
+
   validates :name, :email, presence: true, uniqueness: true
 end

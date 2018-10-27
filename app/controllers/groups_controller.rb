0b8d5364b1411491c3c97e70dbfcf82bad5e70b6
kajiya-satsuki <shupeen.soykiki@gmail.com>
2018-10-27 14:04:08 +0900

    added edit function and commonized groups/new and groups/edit form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
   def index
   end
   
@@ -12,7 +13,7 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to :root, notice: 'グループを作成しました'
     else
-      render :new, alert: 'グループ作成に失敗しました'
+      render :new
     end
   end
 
@@ -20,10 +21,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end

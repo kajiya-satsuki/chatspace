ec0d5dd9be81410b86ce2eca00851b4998ecfacd
kajiya-satsuki <shupeen.soykiki@gmail.com>
2019-04-10 20:28:44 +0900

    deleted the commit of devise folder

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 describe MessagesController do
-  let(:group) {create(:group)}
   let(:user) {create(:user)}
+  let(:group) {create(:group)}
 
   describe 'GET #index' do
 
     context 'log in' do
       before do
         login_user(user)
+        
         get :index, params: {group_id: group.id}
       end
 
@@ -15,15 +16,11 @@ describe MessagesController do
         expect(assigns(:message)).to be_a_new(Message)
       end
 
-      it "assigns the requested messages to @messages" do
-        expect(assigns(:messages)).to eq messages
-      end
-
       it "assigns the requested group to @group" do
         expect(assigns(:group)).to eq group
       end
 
-      it "reders the :index template" do
+      it "renders the :index template" do
         expect(response).to render_template :index
       end
 
